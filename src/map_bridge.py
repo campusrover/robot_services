13ae140df06a6a11b78cc282688ecda4c93f07e8
Nate Dimick <natdimick@gmail.com>
2020-05-03 12:43:24 -0400

    removed line skew bug during merge

--- src/map_bridge.py
+++ src/map_bridge.py
@@ -26,15 +26,15 @@ from PIL import Image, ImageDraw
 from tavares_padilha_line_merge import Point, Line_Segment, merge_lines, convert_coords
 from math import pi
 
-def find_close_lines(l, close_lines, line_hash, distance_diff, angle_diff):
+def find_close_lines(l, close_lines, line_hash, distance_diff, angle_diff, og_angle):
     nearby = l.nearby_points(distance_diff)
     new_close_lines = []
     for n in nearby:
         if line_hash.get(n, None):
             for cl in line_hash[n]:
                 # if there is another segment with a similar enough slope that is close enough to the line, then pair them together and put them in a queue to be merged.
-                larger_theta = max(cl.theta, l.theta)
-                smaller_theta = min(cl.theta, l.theta)
+                larger_theta = max(cl.theta, og_angle)
+                smaller_theta = min(cl.theta, og_angle)
                 alt_theta = smaller_theta + pi
                 theta_diff = min(larger_theta - smaller_theta, alt_theta - larger_theta)
                 if  theta_diff < angle_diff and cl != l and cl not in close_lines and cl not in new_close_lines: 
@@ -64,31 +64,23 @@ def consolodate_lines(lines, distance_diff, angle_diff, min_len):
             l = ls[i]
             close_lines = [l]
             # TODO: put close lines in here, and find close lines of close lines too
-            close_lines += find_close_lines(l, close_lines, line_hash, distance_diff, angle_diff)
+            close_lines += find_close_lines(l, close_lines, line_hash, distance_diff, angle_diff, l.theta)
             close_lines.remove(l)
             if close_lines:
                 # find the close lines of the close lines to l
                 for cl in close_lines:
-                    close_lines += find_close_lines(cl, close_lines, line_hash, distance_diff, angle_diff)
+                    close_lines += find_close_lines(cl, close_lines, line_hash, distance_diff, angle_diff, l.theta)
                 # sort the close lines based on angle difference, then length
                 close_lines = sorted(close_lines, key=lambda x: (abs(x.theta - l.theta), -x.length))
-                #print(sorted(close_lines, key=lambda x: x.id))
                 merge_queue.append((l, close_lines))
-                #print(l, close_lines[0])
                 if l in close_lines:
                     close_lines.remove(l)
                 # remove l from hash and list
-                #print(l, "<-- this is the primary line")
                 line_hash[l.point1].remove(l)
                 line_hash[l.point2].remove(l)
                 ls.pop(i)
                 # remove all the lines l will be merged with from the hash and list
                 for merge_pal in close_lines:
-                    """
-                    print(merge_pal)
-                    print(line_hash[merge_pal.point1])
-                    print(line_hash[merge_pal.point2])
-                    """
                     line_hash[merge_pal.point1].remove(merge_pal)
                     line_hash[merge_pal.point2].remove(merge_pal)
                     ls.pop(ls.index(merge_pal))
@@ -97,25 +89,20 @@ def consolodate_lines(lines, distance_diff, angle_diff, min_len):
                 i += 1
     # 4. go through the queue of pairs to be merged and merge them. put the resulting line segments back into hash and list. 
         if merge_queue:
-            #print("merges in queue: ", len(merge_queue))
             for pair in merge_queue:
                 m = pair[0]
                 for l2 in pair[1]:
-                    #print(m, l2, l2.theta)
                     m = merge_lines(m, l2)
-                    #print(pair[0].id, m.theta)
                 merged = m
                 merged.id = next_id
                 next_id += 1
                 # add new line to the data structures
                 ls.append(merged)
-                #print(merged, merged.theta, " input angles: ", (pair[0].theta, pair[0]), [(k.theta, k) for k in pair[1]])
                 ls = sorted(ls, key=lambda x: x.length, reverse=True)
                 line_hash[merged.point1] = line_hash.get(merged.point1, []) + [merged]
                 line_hash[merged.point2] = line_hash.get(merged.point2, []) + [merged]
             # empty the queue
             merge_queue = []
-            #print("remaining line segments: ", len(ls))
     # 5. repeat 3 and 4 until an iteration yeilds no pairs to be merged. 
         else:
             keep_merging = False
@@ -205,7 +192,8 @@ def map_cb(msg):
         f.write(str(package))
     print("---")
     walls = consolodate_lines(walls, dist, pi / 12, 4)
-    draw_map(walls, width, height, map_num=2)  # TODO remove this once this is stable
+    pixel_walls = [[round(x) for x in l] for l in walls]
+    draw_map(pixel_walls, width, height, map_num=2)  # TODO remove this once this is stable
     walls = convert_coords(walls, (origin.position.x, origin.position.y), (width, height), res, (map_shift, map_rot))
     
     
--- src/tavares_padilha_line_merge.py
+++ src/tavares_padilha_line_merge.py
@@ -3,8 +3,8 @@ import math
 
 class Point():
     def __init__(self, point_2tuple):
-        self.x = round(point_2tuple[0])
-        self.y = round(point_2tuple[1])
+        self.x = point_2tuple[0]
+        self.y = point_2tuple[1]
 
     def distance(self, other_point):
         # returns the distance between this point and the given point
@@ -42,9 +42,12 @@ class Line_Segment():
             self.theta = math.pi
         self.perpindicular = (self.theta + math.pi / 2) % math.pi
 
-    def fourtuple(self):
+    def fourtuple(self, rounded=False):
         # returns the 4-tuple that was passed into to creat the object. 
-        return [self.point1.x, self.point1.y, self.point2.x, self.point2.y]
+        ft = [self.point1.x, self.point1.y, self.point2.x, self.point2.y]
+        if rounded:
+            ft = [round(x) for x in ft]
+        return ft
 
     def nearby_points(self, radius, res=8):
         # finds all points close to this line segment
@@ -102,16 +105,15 @@ def merge_lines(l1, l2):
     gy = (l1.length * (l1.point1.y + l1.point2.y) + l2.length * (l2.point1.y + l2.point2.y)) / (2 * (l1.length + l2.length))
     centroid = Point((gx, gy))
     # define the orientation of the new merged line
-    if abs(l1.theta - l2.theta) <= math.pi / 2:
-        theta = (l1.length * l1.theta + l2.length * l2.theta) / (l1.length + l2.length)
-    else:
-        try:
-            t2 = (l2.theta + math.pi * (l2.theta / abs(l2.theta)))
-            # in line below, had to change "l2.theta - math.pi" to + to resolve a severe line skewing problem
-            theta = (l1.length * l1.theta + l2.length * t2) / (l1.length + l2.length)  # maybe add immensely small number to l2.theta in case l2.theta=0
-        except:
-            print(l2.theta, l2.id, l1.length + l2.length)
-            exit()
+    # this section of the algorithm does it slightly differently than exactly how T+P's paper describes, but the result is the same - a weighted average of the line's angles based on length
+    t1 = l1.theta
+    t2 = l2.theta
+    if abs(l1.theta - l2.theta) > math.pi / 2:
+        if l2.theta > l1.theta:
+            t2 = (l2.theta - math.pi)
+        else:
+            t1 = (l1.theta - math.pi)
+    theta = (l1.length * t1 + l2.length * t2) / (l1.length + l2.length)
     # convert all coordinates to a new frame centered around the cnetroid at angle theta
     # a and b belong to l1, c and d belong to l2.
     # only x converrstions are required 
@@ -165,14 +167,34 @@ def convert_coords(lines, origin, size, res, tf):
     return rot_lines
 
 
+def TP_angle_test(t1, t2):
+    # small test-case of lines 108-117
+    # lengths assumes to be = 1 for both lines
+    if abs(t1 - t2) <= math.pi / 2:
+        t = t1 + t2 / 2
+        print('A')
+    else:
+        t2 += 0.0000000000000000000000000001
+        t2 = (t2 - math.pi * (t2 / abs(t2))) % math.pi
+        t = t1 + t2 / 2
+        print('B')
+        print(t2)
+    return t
+
 if __name__ == '__main__':
     # scrap script space to test that parts are working
+    for a, b in [(3.14, 0), (3.04, .1), (2.94, .2)]:
+        print(TP_angle_test(a, b))
+    """
     a = Line_Segment([312,148,164,148])
-    b = Line_Segment([283,151,294,150])
+    b = Line_Segment([283,151,294,151])
+    b.theta = 0
     print(a.length, b.length)
     print(a.theta, b.theta)
     c = merge_lines(a,b)
-    print(c, c.theta)
+    print("THIS IS THE RESULTING ANGLE:")
+    print(c.theta)
+    """
     """
     a1 = Line_Segment([10,10,20,12])
     a2 = Line_Segment([10,12,20,10])
